67166a9dcc1c5f011a27d12b3573f9aa29a37708
Govaski <joaopedrogovaski@gmail.com>
2025-07-29 16:02:48 -0300

    Fixing hability creation glitch

--- backend/pages/debugHabilities.js
+++ backend/pages/debugHabilities.js
@@ -7,7 +7,36 @@ const habLista = document.getElementById("habLista")
 
 
 window.addEventListener("DOMContentLoaded", async () => { 
-const url = "http://localhost:4000/api/character/get/1"
+    renderHabs()
+})
+
+btnHabCriar.addEventListener("click", async () => {
+
+    let saveHabs = {habs: []}
+    habLista.childNodes.forEach(element => {
+        let nome = element.childNodes[0].textContent.split(':')[1]
+        let desc = element.childNodes[1].textContent.split(':')[1]
+        let dices = element.childNodes[2].textContent.split(':')[1]
+        let habObj = {nome: nome, descricao: desc, dices: dices}
+        saveHabs.habs.push(habObj)
+    });
+
+    saveHabs.habs.push({nome: habNome.value, descricao: habDesc.value, dices: habDados.value})
+
+    const url = "http://localhost:4000/api/character/updateHability/1"
+    await fetch(url, {
+        method:'PUT', 
+        headers: {"Content-Type": "application/json"}, 
+        body: JSON.stringify(saveHabs) 
+    })
+
+    renderHabs()
+})
+
+async function renderHabs(){
+    habLista.innerHTML = ""
+
+    const url = "http://localhost:4000/api/character/get/1"
     await fetch(url, {method:'GET'})
     .then(res => res.json())
     .then(json => json.data[0])
@@ -15,41 +44,30 @@ const url = "http://localhost:4000/api/character/get/1"
         nome.textContent = character.nome
 
         let habilidades = JSON.parse(character.habilidades)
-        
+
         habilidades.forEach(hab => {
             let li = document.createElement('li')
-            li.textContent = `Nome: ${hab.nome} Descrição: ${hab.descricao} Dados: ${hab.dices}`
-            
+
+            let pNome = document.createElement('p')
+            pNome.textContent = `Nome: ${hab.nome}`
+            li.appendChild(pNome)
+
+            let pDesc = document.createElement('p')
+            pDesc.textContent = `Descrição: ${hab.descricao}`
+            li.appendChild(pDesc)
+            let pDice = document.createElement('p')
+            pDice.textContent = `Dados: ${hab.dices}`
+            li.appendChild(pDice)
+
             let btn = document.createElement('button')
             btn.textContent = "Rodar"
             btn.addEventListener('click', () => {
                 let result = rollDicesFromString(hab.dices)
                 console.log(result)
             })
-            
+
             li.appendChild(btn)
             habLista.appendChild(li)
         });
-
-        
     })
-})
-
-btnHabCriar.addEventListener("click", async () => {
-    const url = "http://localhost:4000/api/character/updateHability/1"
-    await fetch(url, {
-        method:'PUT', 
-        headers: {"Content-Type": "application/json"}, 
-        body: JSON.stringify({
-            habs: [
-                {
-                nome: habNome.value,
-                descricao: habDesc.value,
-                dices: habDados.value
-                }
-            ]
-        }) 
-    })
-})
-
-
+}
